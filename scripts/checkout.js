b8381bb0277b2469612e328488340f41fbfdf36f
SuperSimpleDev <prakharpatel0351@gmail.com>
2025-08-25 03:16:09 +0530

    Delete products from cart when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,6 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
-import(formatCurrency)
 let cartSummeryHTML='';
 cart.forEach((cartItem)=> {
   const productId=cartItem.productId;
@@ -38,7 +37,7 @@ cart.forEach((cartItem)=> {
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
@@ -95,3 +94,11 @@ cart.forEach((cartItem)=> {
 
 document.querySelector('.js-order-summary')
   .innerHTML=cartSummeryHTML;
+
+document.querySelectorAll('.js-delete-link')
+  .forEach((link)=>{
+    link.addEventListener('click', ()=>{
+      const productId=link.dataset.productId;
+      removeFromCart(productId);
+    });
+  });
