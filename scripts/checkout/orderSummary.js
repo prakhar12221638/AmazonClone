f6e768ebff0b722913ef0380733452e0929956e4
SuperSimpleDev <prakharpatel0351@gmail.com>
2025-08-28 01:31:11 +0530

    Create automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -28,45 +28,48 @@ export function renderOrderSummary(){
     );
 
     cartSummeryHTML+=`
-      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
-        <div class="delivery-date">
-          Delivery date: ${dateString}
-        </div>
-
-        <div class="cart-item-details-grid">
-          <img class="product-image"
-            src="${matchingProduct.image}">
-
-          <div class="cart-item-details">
-            <div class="product-name">
-              ${matchingProduct.name}
-            </div>
-            <div class="product-price">
-              $${formatCurrency(matchingProduct.priceCents)}
-            </div>
-            <div class="product-quantity">
-              <span>
-                Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-              </span>
-              <span class="update-quantity-link link-primary">
-                Update
-              </span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
-                Delete
-              </span>
-            </div>
+      <div class="cart-item-container 
+        js-cart-item-container
+        js-cart-item-container-${matchingProduct.id}">
+          <div class="delivery-date">
+            Delivery date: ${dateString}
           </div>
 
-          <div class="delivery-options">
-            <div class="delivery-options-title">
-              Choose a delivery option:
+          <div class="cart-item-details-grid">
+            <img class="product-image"
+              src="${matchingProduct.image}">
+
+            <div class="cart-item-details">
+              <div class="product-name">
+                ${matchingProduct.name}
+              </div>
+              <div class="product-price">
+                $${formatCurrency(matchingProduct.priceCents)}
+              </div>
+              <div class="product-quantity
+                js-product-quantity-${matchingProduct.id}">
+                <span>
+                  Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                </span>
+                <span class="update-quantity-link link-primary">
+                  Update
+                </span>
+                <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
+                  Delete
+                </span>
+              </div>
             </div>
-            ${deliveryOptionsHTML(matchingProduct, cartItem)}
+
+            <div class="delivery-options">
+              <div class="delivery-options-title">
+                Choose a delivery option:
+              </div>
+              ${deliveryOptionsHTML(matchingProduct, cartItem)}
+            </div>
           </div>
         </div>
-      </div>
-    `;
-  });
+      `;
+    });
 
   function deliveryOptionsHTML(matchingProduct, cartItem){
 
